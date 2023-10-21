db2c05fb91bb59a880899dd4bb2f028e16a4042d
nishikawa244 <kameru.plus@gmail.com>
2023-10-21 22:22:54 +0900

    Revert "pickupprojectの追加"
    This reverts commit 919bb50984c97fdb53cc5d4938187e16e0dbd98f.

--- pickupapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PickupappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pickupapp'
--- pickupapp/constants.py
+++
@@ -1,2 +0,0 @@
-CAR_CHOICES = ((v, v) for v in ['ハイエース 定員七人', 'セレナ 定員五人', 'タント 定員三人', 'アルト白 定員三人', 'アルト黄 定員三人'])
-PLACE_NUMBER_CHOICES = [(i, str(i)) for i in range(6)]
